7d5625ba6fc813fe3d9535d4731cee66993c8abd
Vasilia Staykova <someonemyfriend@gmail.com>
2024-12-30 00:48:49 +0200

    Add files via upload

---
+++ Accordion/main.js
@@ -0,0 +1,92 @@
+const data = [
+    {
+        id : '1',
+        question: "What are accordion components?",
+        answear: "Accordion components are user interface elements"
+    },
+    {
+        id : '2',
+        question: "What are they used for?",
+        answear: "Accordion components are user interface elements"
+    },
+    {
+        id : '3',
+        question: "Accordion as a musical instrument",
+        answear: "Accordion components are user interface elements"
+    },
+    {
+        id : '4',
+        question: "What4 are accordion components?",
+        answear: "Accordion components are user interface elements"
+    }, {
+        id : '5',
+        question: "What5 are accordion components?",
+        answear: "Accordion components are user interface elements"
+    },
+    {
+        id : '6',
+        question: "What6 are accordion components?",
+        answear: "Accordion components are user interface elements"
+    },
+
+];
+
+const accordionWrapper = document.querySelector('.accordion');
+
+function createAccordionDate() {
+    accordionWrapper.innerHTML = data.map(dataItem =>`
+    <div class="accordion_item">
+    <div class="accordion_title">
+    <h3>${dataItem.question}</h3>
+    <i class="fa-solid fa-arrow-up"></i>
+    </div>
+    <div class="accordion_content">
+    <p>${dataItem.answear}</p>
+    </div>
+    </div>
+    `).join("");
+} 
+
+createAccordionDate();
+
+const getAccordionTitle = document.querySelectorAll('.accordion_title');
+
+console.log("==================================================");
+console.log(getAccordionTitle);
+console.log("==================================================");
+
+getAccordionTitle.forEach(currentItem => {
+    currentItem.addEventListener('click', (event)=> {
+        if (currentItem.classList.remove("active")) {
+            currentItem.classList.remove("active")
+        } else {
+            let getAlreadyAddedActiveClass = document.querySelectorAll('.active');
+
+            getAlreadyAddedActiveClass.forEach(currentActiveItem => {
+                currentActiveItem.classList.remove()
+            })
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
